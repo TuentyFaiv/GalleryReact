754cbbda5205f39e0485d4abeac1b9d3745b643d
Tonalli López <aguilablanca_7@live.com>
2022-01-12 10:24:02 -0600

    🚚 REFACTOR: Structure folders and files

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export function sayHello(name: string): string {
-  return `Hey ${name}, say hello to TypeScript`;
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.scss";
+import "./pages/styles/index.scss";
 
-import App from "./App";
+import App from "./pages/App";
 
 ReactDOM.render(
   <React.StrictMode>
