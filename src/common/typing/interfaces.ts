c1dcebb1b6f6efa4eb6df4286236160c75f431ec
Tonalli López <aguilablanca_7@live.com>
2022-11-20 11:56:59 -0600

    REFACTOR: Change types to interfaces

--- src/common/typing/interfaces.ts
+++ src/common/typing/interfaces.ts
@@ -1,7 +1,29 @@
 import type { Node } from "../structures";
+import type { Alignment } from "./types";
 
 export interface LinkedList<T> {
   head: Node<T>;
   tail: Node<T>;
   length: number;
 }
+
+export interface ID {
+  id: number;
+}
+
+export interface Photo extends ID {
+  src: string;
+  alt: string;
+  srcSets?: PhotoResponsive[];
+}
+
+export interface PhotoResponsive {
+  mediaQuery: string;
+  source: string;
+}
+
+export interface CarouselConfig {
+  navigation?: Alignment;
+  modal?: boolean;
+}
+
